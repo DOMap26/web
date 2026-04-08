57a4cf791543dbd1117d8c44dd008d90e4f78ce5
한승환 <coderrornotfound@gmail.com>
2026-04-09 07:27:09 +0900

    feat: 나무 위치 보정

--- src/map.js
+++ src/map.js
@@ -12,3 +12,20 @@ for (let y = 0; y < mapHeight; y++) {
     map.appendChild(tile);
   }
 }
+
+const trees = [
+  { x: 3, y: 3 },
+  { x: 7, y: 5 },
+  { x: 10, y: 2 },
+];
+
+trees.forEach(({ x, y }) => {
+  const tree = document.createElement("img");
+  tree.src = "./assets/images/tree.png";
+  tree.classList.add("tree");
+
+  tree.style.left = x * tileSize + (tileSize - 80) / 2 + "px";
+  tree.style.top = y * tileSize + (tileSize - 80) / 2 + "px";
+
+  map.appendChild(tree);
+});
